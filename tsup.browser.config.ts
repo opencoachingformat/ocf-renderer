bc94b59b1f4e80006167b5963c45e0b998bafeb5
Oliver-Marcus Eder <github.dev@oliver-eder.de>
2026-08-12 19:09:09 +0200

    feat: add bundled browser build for ocf-renderer

---
+++ tsup.browser.config.ts
@@ -0,0 +1,9 @@
+import { defineConfig } from "tsup";
+
+export default defineConfig({
+  entry: ["src/index.ts"],
+  format: ["esm"],
+  target: "es2020",
+  outDir: "dist/browser",
+  noExternal: ["three"],
+});
